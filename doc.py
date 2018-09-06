28df3cc1a167a6fc25fd2fe2e4d155c8993232c4
Icebowl <icebowl1967@gmail.com>
2018-09-06 13:47:47 -0700

    Add files via upload

---
+++ doc.py
@@ -0,0 +1,7 @@
+# doc.py cwc
+#https://www.w3schools.com/python/python_syntax.asp
+"""  Multiple line 
+     comment.
+     """
+print("Document what you are doing.")     
+
---
+++ indent.py
@@ -0,0 +1,6 @@
+# indent.py
+print ("hello world")
+if 1 > 0:
+  print("greater than")
+if 0 < 1:
+  print("less than")
